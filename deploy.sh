559476d660d1ed015e658462c5469502eec0eaef
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2025-01-08 11:23:09 +0900

    fix: Deploy.sh file add extra cmds

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 # !bin/bash
 
 # PRODUCTION
-git checkout master
 git reset --hard
+git checkout master
 git pull origin master
 
 npm install yarn -g
 yarn global add serve
 yarn
-# yarn run build
+yarn run build
 pm2 start "yarn run start:prod" --name=ANDYMOBILE-REACT
 
 # DEVELOPMENT
